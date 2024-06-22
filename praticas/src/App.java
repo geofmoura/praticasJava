51c58c5d87e81e27cb7b2b482ba9a2f4fab7208f
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-06-22 19:53:47 -0300

    UPDATE PRATICAS

--- praticas/src/App.java
+++ praticas/src/App.java
@@ -25,6 +25,11 @@ public class App {
 	    System.out.println("MARIO       8");
 	    System.out.println("LUCAS      5.6");
 	    System.out.println("GEOVANA    9.9");
-        
+
+        System.out.println("L");
+        System.out.println("L");
+        System.out.println("L");
+        System.out.println("LLLLL");
+
     }
 }
