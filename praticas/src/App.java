d65da17923e033fc3ab63f04c21605ba9d199b8b
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-06-22 19:45:10 -0300

    IMPRIMINDO MENSAGEM

---
+++ praticas/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, aqui está as minhas primeiras práticas em Java");
+
+        System.out.println("XXXXXX");
+        System.out.println("X    X");
+        System.out.println("X    X");
+        System.out.println("X    X");
+        System.out.println("XXXXXX");
+
+    //estou com dificuldade para quebrar linhas, por enquanto
+
+        System.out.println("      X");
+	    System.out.println("     XXX");
+	    System.out.println("    XXXXX");
+	    System.out.println("   XXXXXXX");
+	    System.out.println("  XXXXXXXXX");
+	    System.out.println(" XXXXXXXXXXX");
+	    System.out.println("      XX    ");
+        
+    }
+}
