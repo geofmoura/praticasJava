ede1374d9cec73a61f7b222516ffc8688fd831aa
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-07-02 15:42:01 -0300

    scanner com improt sem variaveis

--- praticas/src/Entrada.java
+++ praticas/src/Entrada.java
@@ -4,18 +4,12 @@ import java.util.Calendar;
 public class Entrada {
     public static void main(String[]args){
         Scanner entrada  = new Scanner (System.in);
-        int idade;
-        int ano_atual;
-        int ano_nascimento;
+
 
         System.out.println("Digite sua idade: ");
-        idade = entrada.nextInt();
-        
         Calendar calendario = Calendar.getInstance();
-        ano_atual=calendario.get(Calendar.YEAR);
-        ano_nascimento=ano_atual - idade;
-
-        System.out.println("Voce nasceu  em " + ano_nascimento + "\n");
+        
+        System.out.println("Voce nasceu em " + (calendario.get(Calendar.YEAR)- entrada.nextInt()) + "\n");
 
         entrada.close();
     }
