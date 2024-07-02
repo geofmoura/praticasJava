1eef31001b3aa7e31abc0487c6669e2e8678d15d
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-07-02 15:25:14 -0300

    Scanner

---
+++ praticas/src/Entrada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[]args){
+        Scanner entrada  = new Scanner (System.in);
+        int idade;
+
+        System.out.println("Digite sua idade: ");
+        idade = entrada.nextInt();
+        
+        System.out.println("Sua idade é " + idade + "\n");
+
+        entrada.close();
+    }
+}
