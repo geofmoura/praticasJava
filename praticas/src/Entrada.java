63070c7375f230caff96b0071fc46c494a015b98
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-07-02 15:35:38 -0300

    leitura com import

--- praticas/src/Entrada.java
+++ praticas/src/Entrada.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class Entrada {
     public static void main(String[]args){
         Scanner entrada  = new Scanner (System.in);
         int idade;
+        int ano_atual;
+        int ano_nascimento;
 
         System.out.println("Digite sua idade: ");
         idade = entrada.nextInt();
         
-        System.out.println("Sua idade é " + idade + "\n");
+        Calendar calendario = Calendar.getInstance();
+        ano_atual=calendario.get(Calendar.YEAR);
+        ano_nascimento=ano_atual - idade;
+
+        System.out.println("Voce nasceu  em " + ano_nascimento + "\n");
 
         entrada.close();
     }
