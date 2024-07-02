d7f78bdc8478d16bb0588f40964f0d1374f79ad1
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-07-02 18:01:18 -0300

    praticas adicionadas.

--- praticas/src/Entrada.java
+++ praticas/src/Entrada.java
@@ -12,5 +12,24 @@ public class Entrada {
         System.out.println("Voce nasceu em " + (calendario.get(Calendar.YEAR)- entrada.nextInt()) + "\n");
 
         entrada.close();
+
+        System.out.println("Ola, aqui esta as minhas primeiras praticas em Java");
+        
+        System.out.println("XXXXXX");
+        System.out.println("X    X");
+        System.out.println("X    X");
+        System.out.println("X    X");
+        System.out.println("XXXXXX");
+
+    //estou com dificuldade para quebrar linhas, por enquanto
+
+        System.out.println("      X");
+        System.out.println("     XXX");
+        System.out.println("    XXXXX");
+        System.out.println("   XXXXXXX");
+        System.out.println("  XXXXXXXXX");
+        System.out.println(" XXXXXXXXXXX");
+        System.out.println("      XX    ");
+        
     }
 }
